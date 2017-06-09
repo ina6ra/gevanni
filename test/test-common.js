94ee793b81ddf6422b3babd62919f02f5f4d58db
ina6ra <inarinlab@gmail.com>
2017-06-09 09:51:57 +0900

    node-fixtures を使ってテストをリファクタリング

--- test/test-common.js
+++ test/test-common.js
@@ -1,6 +1,7 @@
 var assert = require('chai').assert;
 var Sugar = require('sugar');
 var config = require('config');
+var fx = require('node-fixtures');
 
 // $ yarn add https://github.com/ina6ra/gas-mock
 var gas = require('gas-mock');
@@ -21,31 +22,12 @@ describe('common.js', function() {
   var setup = config.get('Setup');
   var fixture = config.get('Fixture');
 
-  before(function() {
-    // UrlFetchApp.fetch の上書き
-    Sugar.Object.set(mymock, 'UrlFetchApp.fetch', function(url, params) {
-      return this.response;
-    });
-    // Properties.getProperty の上書き
-    Sugar.Object.set(mymock, 'Properties.getProperty', function(key) {
-      return setup['ScriptProperties'][key];
-    });
-    uid = glib.telegram.getUpdateID();
-    token = glib.telegram.getApiToken();
-    method = 'getUpdates';
-    url = glib.telegram.getApiUrl(token, method);
-    // HTTPResponse.getContentText の上書き
-    Sugar.Object.set(mymock, 'HTTPResponse.getContentText', function(charset) {
-      return JSON.stringify(fixture['getUpdates']['Array']);
-    });
-    json = glib.telegram.getUpdates(url);
-  });
-
   // test for createPayloadList()
   describe('#createPayloadList()', function() {
 
     it('should return Array Class', function() {
-      result = glib.common.createPayloadList(json, uid);
+      json = fx.common.createPayloadList.Array;
+      result = glib.common.createPayloadList(json);
       assert.typeOf(result, 'Array');
     });
 
@@ -75,24 +57,14 @@ describe('common.js', function() {
     it('UIDのチェックをしない');
 
     it('空文字のとき結果はゼロ', function() {
-      // HTTPResponse.getContentText の上書き
-      Sugar.Object.set(mymock, 'HTTPResponse.getContentText', function(charset) {
-        json.result = [fixture['createPayloadList']['empty']];
-        return JSON.stringify(json);
-      });
-      json = glib.telegram.getUpdates(url);
-      result = glib.common.createPayloadList(json, uid);
+      json.result = [fx.common.createPayloadList.empty];
+      result = glib.common.createPayloadList(json);
       assert.equal(result.length, 0);
     });
 
     it('プレビューオフ', function() {
-      // HTTPResponse.getContentText の上書き
-      Sugar.Object.set(mymock, 'HTTPResponse.getContentText', function(charset) {
-        json.result = [fixture['createPayloadList']['all']];
-        return JSON.stringify(json);
-      });
-      json = glib.telegram.getUpdates(url);
-      result = glib.common.createPayloadList(json, uid);
+      json.result = [fixture['createPayloadList']['all']]
+      result = glib.common.createPayloadList(json);
       assert.equal(result[0].disable_web_page_preview, true);
     });
 
